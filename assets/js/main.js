bb4fd1e51f5c238b779f748d7d640df2b9483ee9
Cássio Souza <cassiosza.dev@gmail.com>
2025-11-17 19:39:05 -0300

    Final desafio
    Adição do código do desafio proposto, onde acrescentei um popup que no click, abre com os detalhes do pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,9 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
+        <li id="pokemonCard-${pokemon.number}"
+            class="pokemon ${pokemon.type}"
+            onclick="abrirDetalhes(${pokemon.number})">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
@@ -35,13 +37,83 @@ loadPokemonItens(offset, limit)
 loadMoreButton.addEventListener('click', () => {
     offset += limit
     const qtdRecordsWithNexPage = offset + limit
-
+    
     if (qtdRecordsWithNexPage >= maxRecords) {
         const newLimit = maxRecords - offset
         loadPokemonItens(offset, newLimit)
-
+        
         loadMoreButton.parentElement.removeChild(loadMoreButton)
     } else {
         loadPokemonItens(offset, limit)
     }
 })
+
+function abrirDetalhes(pokemonId) {
+    loadMoreButton.style.display = 'none';
+
+    pokeApi.getPokemonDetail(pokemonId).then(pokemonDetail => {
+        const detailHTML = generateDetailHTML(pokemonDetail);
+        let detailContainer = document.getElementById('pokemonDetailContainer');
+        if (!detailContainer) {
+            detailContainer = document.createElement('div');
+            detailContainer.id = 'pokemonDetailContainer';
+            document.body.appendChild(detailContainer);
+        }
+        detailContainer.innerHTML = detailHTML
+        detailContainer.classList.add('active');
+    })
+}
+
+function voltarParaLista() {
+    const detailContainer = document.getElementById('pokemonDetailContainer');
+    if (detailContainer) {
+        detailContainer.classList.remove('active');
+    }
+
+    loadMoreButton.style.display = 'block';
+}
+
+function generateDetailHTML(pokemon) {
+    const getStatValue = (statName) => {
+        const stat = pokemon.stats.find(s => s.name === statName);
+        return stat ? stat.value : 'N/A';
+    };
+
+    const hpStat = getStatValue('hp');
+    const attackStat = getStatValue('attack');
+    
+    return `
+        <section class="pokemon-detail-page ${pokemon.type}">
+            <header>
+                <button class="back-button" onclick="voltarParaLista()">❮ Voltar</button>
+                <h1 class="name">${pokemon.name}</h1>
+                <span class="number">#${pokemon.number}</span>
+            </header>
+
+            <div class="content">
+                <div class="image-area">
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+                </div>
+
+                <div class="info-area">
+                    
+                    <h3>Estatísticas Base</h3>
+                    <ul class="stats-list">
+                        <li>HP: ${hpStat}</li>
+                        <li>Attack: ${attackStat}</li>
+                        </ul>
+                    
+                    <h3>Informações Gerais</h3>
+                    <ul class="general-info">
+                        <li>Altura: ${pokemon.height / 10} m</li>
+                        <li>Peso: ${pokemon.weight / 10} kg</li>
+                        <li>Habilidades: ${pokemon.abilities.join(', ')}</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    `;
+}
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,5 +1,5 @@
-
 const pokeApi = {}
+
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
@@ -17,7 +17,7 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     return pokemon
 }
 
-pokeApi.getPokemonDetail = (pokemon) => {
+pokeApi.getPokemonDetailByUrl = (pokemon) => {
     return fetch(pokemon.url)
         .then((response) => response.json())
         .then(convertPokeApiDetailToPokemon)
@@ -29,7 +29,30 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetailByUrl)) 
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
+}
+
+pokeApi.getPokemonDetail = (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    return fetch(url)
+        .then((response) => response.json())
+        .then((detail) => {
+            const pokemon = new Pokemon();
+
+            pokemon.number = detail.id;
+            pokemon.name = detail.name;
+            pokemon.photo = detail.sprites.other.dream_world.front_default;
+            pokemon.types = detail.types.map((typeSlot) => typeSlot.type.name);
+            pokemon.type = pokemon.types[0];
+            pokemon.height = detail.height;
+            pokemon.weight = detail.weight;
+            pokemon.abilities = detail.abilities.map((abilitySlot) => abilitySlot.ability.name)
+            pokemon.stats = detail.stats.map((statSlot) => ({
+                name: statSlot.stat.name,
+                value: statSlot.base_stat
+            }));
+            return pokemon;
+        })
 }
